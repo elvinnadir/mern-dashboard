6ead7394a59de648331e21ebd365a39086421f39
Elvin <elvinnadir92@gmail.com>
2023-02-18 15:23:45 +0400

    client added

---
+++ client/src/pages/index.ts
@@ -0,0 +1,26 @@
+import Home from './Home'
+import Agents from './Agents'
+import AllProperties from './AllProperties'
+import CreateProperty from './CreateProperty'
+import EditProperty from './EditProperty'
+import {Login} from './login'
+import MyProfile from './MyProfile'
+import PropertyDetails from './PropertyDetails'
+import AgentProfile from './AgentProfile'
+
+
+export
+{
+    Home,
+    Agents,
+    AllProperties,
+    CreateProperty,
+    EditProperty,
+    Login,
+    MyProfile,
+    PropertyDetails,
+    AgentProfile,
+
+}
+
+
